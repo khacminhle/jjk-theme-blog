b13a695cafd661a8ff77a8e724151a7f1c949d51
Minh Le <minh_le_2010@yahoo.com>
2024-02-02 16:29:14 +1100

    modified main and add Procfile

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 from typing import List
 from flask import Flask, abort, render_template, redirect, url_for, flash
@@ -28,7 +29,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -55,7 +56,7 @@ gravatar = Gravatar(app,
                     use_ssl=False,
                     base_url=None)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -268,4 +269,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
